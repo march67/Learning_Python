eee8932ed7ec3a5365d99d71628ce4adb685ebe1
march67 <td.ta.david@gmail.com>
2023-04-17 15:12:21 +0200

    fin learning multiple assignment

---
+++ Learning_Python_03_affectation-multiple.py
@@ -0,0 +1,22 @@
+# affectation individuel
+# nom = "David"
+# age = 21
+# humain = True
+
+# affectation multiple en une ligne de code, séparer par des virgules
+nom, age, humain = "David", 21, True
+
+print(nom)
+print(age)
+print(humain)
+
+# affectation individuel, les variables ont la même valeur
+# Humain1 = 30
+# Humain2 = 30
+# Humain3 = 30
+
+# affectation multiple pour des variables possédant la même valeur
+Humain1 = Humain2 = Humain3 = 30
+print(Humain1)
+print(Humain2)
+print(Humain3)
